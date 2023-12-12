ea4f0d7feeae966f61151b76249e06568e7abd53
Michelle Nguyen <missmichellenguyen01@gmail.com>
2023-12-12 13:50:23 -0800

    updates

---
+++ js/dom-play.js
@@ -0,0 +1,26 @@
+//alert("hello from DOM play!")
+
+//document.querySelector(".hamlet").style.backgroundColor = "orange";
+
+function highlightActor(actor){
+
+    const spans = document.querySelectorAll("div#play span");
+    for(const mySpan of spans){
+        if(mySpan.dataset.actor == actor){
+            mySpan.style.backgroundColor='green';
+        }else{
+            mySpan.style.backgroundColor='white';
+        }
+
+    }
+
+}
+
+const spans = document.querySelectorAll("div#play span");
+console.log(spans);
+
+for(const mySpan of spans){
+    mySpan.addEventListener("click",function(ev){
+        highlightActor(mySpan.dataset.actor);
+    });
+}
